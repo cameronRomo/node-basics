ae568d90f60d9b8dfac2aecaccad197195d870c5
Cameron Romo <cromo1225@gmail.com>
2023-02-07 16:20:59 -0700

    Unblock response

--- app.js
+++ app.js
@@ -20,15 +20,16 @@ const server = http.createServer((req, res) => {
       console.log("chunk", chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
-      fs.writeFileSync("message.txt", message);
+      fs.writeFile("message.txt", message, (err) => {
+        res.writeHead(302, {
+          Location: "/",
+        });
+        return res.end();
+      });
     });
-    res.writeHead(302, {
-      Location: "/",
-    });
-    return res.end();
   }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
