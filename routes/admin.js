6319466ab9e5060a69407d54de41262b13e75b29
Cameron Romo <cromo1225@gmail.com>
2023-02-08 14:24:07 -0700

    Extract to views

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,9 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    `<html>
-      <form action='/admin/add-product' method='POST'>
-        <input type='text' name='title'/>
-        <button type='submit'>Add Product</button>
-      </form>
-    </html>`
-  );
+  res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
