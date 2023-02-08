14eaeb13f96a98c668c82634145671352046eed5
Cameron Romo <cromo1225@gmail.com>
2023-02-08 15:32:51 -0700

    Add styles

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const app = express();
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-const app = express();
-
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
