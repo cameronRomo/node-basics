bae90f07f4a7ee3784726d68e53a3e2d90dc20d9
Cameron Romo <cromo1225@gmail.com>
2023-02-11 10:39:50 -0700

    Register new routes

--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,5 @@
 exports.get404 = (req, res, next) => {
   res
     .status(404)
-    .render("not-found", { pageTitle: "Page Not Found", path: "" });
+    .render("not-found", { pageTitle: "Page Not Found", path: "/not-found" });
 };
