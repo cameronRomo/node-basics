52c8b3cd0bdb6d116eac9182e557bfa281901b52
Cameron Romo <cromo1225@gmail.com>
2023-02-11 09:42:44 -0700

    Utilize the file system package for storing/retrieving data

--- models/product.js
+++ models/product.js
@@ -1,4 +1,5 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +7,37 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, data) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(data);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(require.main.filename),
+      "data",
+      "products.json"
+    );
+
+    fs.readFile(p, (err, data) => {
+      if (err) {
+        cb([]);
+      } else {
+        cb(JSON.parse(data));
+      }
+    });
   }
 };
