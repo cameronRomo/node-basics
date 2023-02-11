b7beb683ff2953efd5d38f8b8d9e46f5fb265e94
Cameron Romo <cromo1225@gmail.com>
2023-02-11 09:54:39 -0700

    Clean up code

--- models/product.js
+++ models/product.js
@@ -1,23 +1,29 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = (cb) => {
+  fs.readFile(p, (err, data) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(data));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, data) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(data);
-      }
+    getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
@@ -26,18 +32,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(require.main.filename),
-      "data",
-      "products.json"
-    );
-
-    fs.readFile(p, (err, data) => {
-      if (err) {
-        cb([]);
-      } else {
-        cb(JSON.parse(data));
-      }
-    });
+    getProductsFromFile(cb);
   }
 };
