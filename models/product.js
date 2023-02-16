f0c63fc672b76fbcb86e42a580efbf2ba8a68489
Cameron Romo <cromo1225@gmail.com>
2023-02-16 10:23:02 -0700

    Add cart model and functionality to add to cart

--- models/product.js
+++ models/product.js
@@ -30,7 +30,7 @@ module.exports = class Product {
     getProductsFromFile((products) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
+        console.error(err);
       });
     });
   }
