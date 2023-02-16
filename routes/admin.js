baca6c174b9c67d69dc33d8298f372013100a050
Cameron Romo <cromo1225@gmail.com>
2023-02-16 12:35:14 -0700

    Add functionality to edit a product

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,8 @@ const adminController = require("../controllers/admin");
 router.get("/add-product", adminController.getAddProduct);
 router.get("/products", adminController.getProducts);
 router.post("/add-product", adminController.postAddProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
 
 module.exports = {
   router,
