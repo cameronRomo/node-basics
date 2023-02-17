aeba33e2af56ccbdce0c7d1ef513b9b2a303fe2a
Cameron Romo <cromo1225@gmail.com>
2023-02-16 17:20:56 -0700

    Add ability to delete from cart

--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,7 @@ router.get("/products", shopController.getProducts);
 router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", shopController.getCart);
 router.post("/cart", shopController.addToCart);
+router.post("/cart-delete-item", shopController.postCartDeleteProduct);
 router.get("/orders", shopController.getOrders);
 router.get("/checkout", shopController.getCheckout);
 
