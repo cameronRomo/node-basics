86cfb72c7ce798f548b00ef4b1524044fee77ced
Cameron Romo <cromo1225@gmail.com>
2023-02-18 11:57:26 -0700

    Connect to mySQL db and define/sync Product model/data

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./util/db");
+const sequelize = require("./util/db");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -12,14 +12,6 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
 
-db.execute("SELECT * FROM products")
-  .then((result) => {
-    console.log("res", result);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -28,4 +20,10 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log("sync result >", result)
+    app.listen(3000);
+  })
+  .catch((err) => console.error(err));
