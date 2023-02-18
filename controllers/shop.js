737d4dbaccb7d4b36d7cfe8730a5314c05902ebb
Cameron Romo <cromo1225@gmail.com>
2023-02-18 12:38:04 -0700

    Find a single product by its id

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,8 +17,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(([[product]]) => {
+  Product.findByPk(prodId)
+    .then((product) => {
       console.log("product", product);
       res.render("shop/product-detail", {
         product,
