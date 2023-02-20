f2f90b64f702e09ee7827a4d40560dfea844406d
Cameron Romo <cromo1225@gmail.com>
2023-02-20 10:57:58 -0700

    Change cart model to utilize sequelize and add many-to-many

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const sequelize = require("./util/db");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cartItem");
 
 const app = express();
 app.set("view engine", "ejs");
@@ -35,10 +37,14 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true }) //For dev purposes only
-  .sync()
+  .sync({ force: true }) //For dev purposes only
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
   })
